7f42c501715ee44286c7a2926dfee1955f30fdce
sanja <sanja@Sanju>
2023-06-06 21:09:25 +0530

    [6th June 23]:- TestNg Annotations & Keywords

---
+++ src/TestNG/example5_TestNGKeyword1_invocationCount.java
@@ -0,0 +1,15 @@
+package TestNG;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class example5_TestNGKeyword1_invocationCount 
+{
+	
+	@Test(invocationCount = 7)
+	public void TC1()
+	{
+		Reporter.log("--running TC1--",true);
+	}
+
+}
