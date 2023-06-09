d06f09ab351ab8563c3eb721c33ad5e33e736db4
sanja <sanja@Sanju>
2023-06-09 21:13:15 +0530

    [9th June 23]:- TestNG Keyword->Suite

--- src/TestNG/example5_TestNGKeyword4_Timeouts.java
+++ src/TestNG/example5_TestNGKeyword4_Timeouts.java
@@ -3,12 +3,13 @@ package TestNG;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
-public class example5_TestNGKeyword1_invocationCount 
+public class example5_TestNGKeyword4_Timeouts
 {
 	
-	@Test(invocationCount = 7)
-	public void TC1()
+	@Test(timeOut = 5000)
+	public void TC1() throws InterruptedException
 	{
+		Thread.sleep(3000);
 		Reporter.log("--running TC1--",true);
 	}
 
